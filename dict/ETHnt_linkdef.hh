e5fe0045c71b9f3f2486b7d1b04656cf2ecb1a11
casal <>
2010-11-18 11:03:55 +0000

    linkdef for ETHnt

---
+++ dict/ETHnt_linkdef.hh
@@ -0,0 +1,10 @@
+#if defined(__CINT__) 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class ETHnt;
+#pragma link C++ class ETHMisc;
+#pragma link C++ class ETHJet;
+#endif
+
