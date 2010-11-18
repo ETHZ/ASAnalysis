58e79ccff0ee96afb6257761c94f6bd456ba61f6
casal <>
2010-11-18 12:52:34 +0000

    linkdef for MT2tree

---
+++ dict/MT2tree_linkdef.hh
@@ -0,0 +1,10 @@
+#if defined(__CINT__) 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class MT2tree;
+#pragma link C++ class MT2Misc;
+#pragma link C++ class MT2Jet;
+#endif
+
