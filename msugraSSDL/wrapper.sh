78db41f81fa475aca104f1545733c80e4e9b90fb
peruzzi <>
2012-01-18 12:55:54 +0000

    This commit was manufactured by cvs2svn to create tag 'V02-06-02'.
    Sprout from master 2012-01-18 12:55:53 UTC buchmann 'Added x value for scans'
    Delete:
        msugraSSDL/ExclusionPlot.C
        msugraSSDL/ExclusionPlot.hh
        msugraSSDL/nlo_kfactors.txt
        msugraSSDL/plot.c
        msugraSSDL/run_scan.c
        msugraSSDL/start.py
        msugraSSDL/wrapper.sh
        msugraSSDL/xsec_lo.txt

--- msugraSSDL/wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /swshare/ROOT/root_v5.30.00_40807_slc5_amd64//bin/thisroot.sh
-
-cd /shome/mdunser/workspace/CMSSW_4_2_8/src/DiLeptonAnalysis/NTupleProducer/macros/msugraSSDL
-
-#root -l -b -q /shome/mdunser/workspace/CMSSW_4_2_8/src/DiLeptonAnalysis/NTupleProducer/macros/msugraScan/run_scan.c\(${1},${2}\)
-root -l -b -q run_scan.c\(${1}\)
