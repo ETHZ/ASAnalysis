46a2b3fc3145cc56960b0834de127f70fa2f16d6
mdunser <>
2012-02-06 19:40:09 +0000

    adapted plot script for the new limit setting tool

--- msugraSSDL/plot.c
+++ msugraSSDL/plot.c
@@ -1,144 +1,127 @@
 {
-//-----------------------------------------------------------------------------------------------------------------------------------
-// this shared library which is included below contains the template msugra exculsion plot. 
-// it can be obtained by following the instructions here: https://twiki.cern.ch/twiki/bin/view/CMS/SUSYLimitTools2011
-// note however that i (marc) adapted the given macro. it didn't work first and also the axes and all that is slightly different
-// so i suggest you use the version which is checked into cvs
-//-----------------------------------------------------------------------------------------------------------------------------------
-gSystem->Load("./ExclusionPlot_C.so");
-//-----------------------------------------------------------------------------------------------------------------------------------
-// all the files which will be used should be present at runtime. 
-// this can be achieved by running the SSDLPlotter with the according functions enabled.
-//-----------------------------------------------------------------------------------------------------------------------------------
-
 // get LO cross sections out of file
-TFile * res_ = new TFile("res.root", "READ", "res_");
+TFile * res_ = new TFile("results_HT450_MET120_PT20_10.root", "READ", "res_");
 res_->cd();
-TH2D * xsec_lo_ = (TH2D *) res_->Get("lo_xsec");
+TH2D * xsec_nlo_ = (TH2D *) res_->Get("nlo_xsec");
+TH2D * yield_ = (TH2D *) res_->Get("msugra_yield");
 
-int m0_bins(140), m0_min(220), m0_max(3000);
-int m12_bins(46), m12_min(100), m12_max(1000);
+// smoothing the yields (physical but ugly)
+int nYieldSmooths(0);
+// smoothing the limit (unphysical but pretty)
+int nContSmooths(0);
 
-// get the calculated limits in the form of TH2Ds
-TFile * file_ = new TFile("limit.root", "READ", "file_");
-file_->cd();
-TH2D * obs_limit_    = (TH2D *) file_->Get("msugra_obslimit");
-TH2D * exp_limit_    = (TH2D *) file_->Get("msugra_explimit");
-TH2D * explo_limit_  = (TH2D *) file_->Get("msugra_explo");
-TH2D * exphi_limit_  = (TH2D *) file_->Get("msugra_exphi");
+while (nYieldSmooths > 0) {
+	yield_   -> Smooth();
+	nYieldSmooths--;
+}
 
-TH2D * obs_contour   = new TH2D ("obs_exclusion"   , "obs_exclusion"   , m0_bins , m0_min-10 ,m0_max+10 , m12_bins , m12_min-10 , m12_max+10);
-TH2D * exp_contour   = new TH2D ("exp_exclusion"   , "exp_exclusion"   , m0_bins , m0_min-10 ,m0_max+10 , m12_bins , m12_min-10 , m12_max+10);
-TH2D * explo_contour = new TH2D ("explo_exclusion" , "explo_exclusion" , m0_bins , m0_min-10 ,m0_max+10 , m12_bins , m12_min-10 , m12_max+10);
-TH2D * exphi_contour = new TH2D ("exphi_exclusion" , "exphi_exclusion" , m0_bins , m0_min-10 ,m0_max+10 , m12_bins , m12_min-10 , m12_max+10);
+int m0_bins(150) , m0_min(0)  , m0_max(3000);
+int m12_bins(50) , m12_min(0) , m12_max(1000);
 
-for (int m0bin = 1; m0bin<=m0_bins; m0bin++){
-	for (int m12bin = 1; m12bin<=m12_bins; m12bin++){
-		float lo  = xsec_lo_->GetBinContent(m0bin, m12bin);
-		int m0 = m0_min + (m0bin-1)*20;
-		int m12 = m12_min + (m12bin-1)*20;
+// the following limits are from the LandS tool, insert your numbers here
+float observed = 6.07407;
+float expected = 6.32644;
+float exphi    = 3.74196;
+float explo    = 13.5196;
 
-		//if ( m12 > 500 || ( m0 > 1000 && m12 > 400) ) {
-		//	if (lo < obs) obs_contour->Fill(m0, m12, -1);
-		//	if (lo < exp) exp_contour->Fill(m0, m12, -1);
-		//	if (lo < explo) explo_contour->Fill(m0, m12, -1);
-		//	if (lo < exphi) exphi_contour->Fill(m0, m12, -1);
-		//	continue;
-		//}
-		//float lo  = 1E9*xsec_lo_->GetBinContent(m0/20., m12/20.);
-		//float lo  = xsec_lo_->GetBinContent(m0/20., m12/20.);
-// observed limit
-		float obs = obs_limit_->GetBinContent(m0bin, m12bin);
-		if (lo > obs && obs!=0) obs_contour->Fill(m0, m12, 1);
-		if (lo < obs) obs_contour->Fill(m0, m12, -1);
-// expected limit
-		float exp = exp_limit_->GetBinContent(m0bin, m12bin);
-		if (lo > exp && exp!=0) exp_contour->Fill(m0, m12, 1);
-		if (lo < exp) exp_contour->Fill(m0, m12, -1);
-// expected -1 sigma
-		float explo = explo_limit_->GetBinContent(m0bin, m12bin);
-		if (lo > explo && explo!=0) explo_contour->Fill(m0, m12, 1);
-		if (lo < explo) explo_contour->Fill(m0, m12, -1);
-// expected +1 sigma
-		float exphi = exphi_limit_->GetBinContent(m0bin, m12bin);
-		if (lo > exphi && exphi!=0) exphi_contour->Fill(m0, m12, 1);
-		if (lo < exphi) exphi_contour->Fill(m0, m12, -1);
+
+TH2D * obs_contour   = new TH2D ("obs_exclusion"   , "obs_exclusion"   , m0_bins , m0_min, m0_max , m12_bins, m12_min, m12_max);
+TH2D * exp_contour   = new TH2D ("exp_exclusion"   , "exp_exclusion"   , m0_bins , m0_min, m0_max , m12_bins, m12_min, m12_max);
+TH2D * explo_contour = new TH2D ("explo_exclusion" , "explo_exclusion" , m0_bins , m0_min, m0_max , m12_bins, m12_min, m12_max);
+TH2D * exphi_contour = new TH2D ("exphi_exclusion" , "exphi_exclusion" , m0_bins , m0_min, m0_max , m12_bins, m12_min, m12_max);
+
+for (int m0 = 20; m0 <= m0_max; m0+=20){
+	for (int m12 = 20; m12 <= m12_max; m12+=20){
+		int bin = yield_->FindBin(m0, m12);
+		float yield = yield_->GetBinContent(bin);
+		if (yield > observed) {
+			obs_contour->Fill(m0, m12, 1);
+		}
+		if (yield > expected) {
+			exp_contour->Fill(m0, m12, 1);
+		}
+		if (yield > exphi) {
+			exphi_contour->Fill(m0, m12, 1);
+		}
+		if (yield > explo) {
+			explo_contour->Fill(m0, m12, 1);
+		}
 	}
 }
 
-TFile * outfile = new TFile("plots.root", "RECREATE", "");
+TFile * outfile = new TFile("plots_crossCheck.root", "RECREATE", "");
 obs_contour->Write();
 exp_contour->Write();
 explo_contour->Write();
 exphi_contour->Write();
 
-// filling the holes in the plots COSMETICS
-//for (int m0 = 20; m0<= 2000; m0+=20){
-//	for (int m12 = 20; m12<= 760; m12+=20){
-//		int m0bin  = m0/20;
-//		int m12bin = m12/20;
-//		if ( (m12 < 150) || (m12 < 240 && m0 <1000)  || (m12 < 300 && m0 <600)  || (m12 < 200 && m0 <800) || (m12 < 200 && m0 <1300) || (m12 < 250 && m0 > 800 && m0 < 1050) )
-//			obs_contour->SetBinContent(m0bin, m12bin, 1);
-//		if ( (m12 < 150) || (m12 < 200 && m0 <1050)  || (m12 < 300 && m0 <600) || (m12 < 200 && m0 <1400) )
-//			exp_contour->SetBinContent(m0bin, m12bin, 1);
-//		if ( (m12 < 150) || (m12 < 250 && m0 <1400)  || (m12 < 320 && m0 <600) )
-//			explo_contour->SetBinContent(m0bin, m12bin, 1);
-//		if ( (m12 < 150) || (m12 < 200 && m0 <1050)  || (m12 < 300 && m0 <500) )
-//			exphi_contour->SetBinContent(m0bin, m12bin, 1);
-//	}
-//}
 
-//int n_smooths(1);
-//
-//while (n_smooths > 0) {
-//	obs_contour   -> Smooth();
-//	exp_contour   -> Smooth();
-//	explo_contour -> Smooth();
-//	exphi_contour -> Smooth();
-//	n_smooths--;
-//}
-//
-//obs_contour->Draw("cont list");
-//gPad.Update();
-//TList * obj   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
-//TList * list  = (TList *) obj->At(10);
-//TGraph * grph = (TGraph*) list->First()->Clone();
+// ----------- COSMETICS --------------
+// filling the holes in the plots
+// ------------------------------------
+for (int m0 = 20; m0 <= m0_max; m0+=20){
+	for (int m12 = 20; m12 <= m12_max; m12+=20){
+		int bin = obs_contour->FindBin(m0-10, m12-10);
+		if ( (m12 < 200 && m0<1000) || (m0 < 600 && m12 < 300) || (m12 < 220) || (m0 > 2200 && m12 < 220) || (m0 > 2500 && m12 < 260)) {
+			obs_contour->SetBinContent  (bin , 1);
+			exp_contour->SetBinContent  (bin , 1);
+			explo_contour->SetBinContent(bin , 1);
+			exphi_contour->SetBinContent(bin , 1);
+		}
+	}
+}
 
-//exp_contour->Draw("cont list");
-//gPad.Update();
-//TList * obj_exp   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
-//TList * list_exp  = (TList *) obj_exp->At(10);
-//TGraph * grph_exp = (TGraph*) list_exp->First()->Clone();
-//
-//explo_contour->Draw("cont list");
-//gPad.Update();
-//TList * obj_explo   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
-//TList * list_explo  = (TList *) obj_explo->At(10);
-//TGraph * grph_explo = (TGraph*) list_explo->First()->Clone();
-//
-//exphi_contour->Draw("cont list");
-//gPad.Update();
-//TList * obj_exphi   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
-//TList * list_exphi  = (TList *) obj_exphi->At(10);
-//TGraph * grph_exphi = (TGraph*) list_exphi->First()->Clone();
 
-//gStyle->SetOptStat(0);
-//TCanvas * canv;
-//canv = ExclusionPlot();
-//grph->SetLineColor(2);
-//grph->SetLineWidth(3);
-//
-//grph_exp->SetLineColor(1);
-//grph_exp->SetLineWidth(3);
-//
-//grph_explo->SetLineColor(1);
-//grph_explo->SetLineWidth(2);
-//grph_explo->SetLineStyle(2);
-//
-//grph_exphi->SetLineColor(1);
-//grph_exphi->SetLineWidth(2);
-//grph_exphi->SetLineStyle(2);
-//
+while (nContSmooths > 0) {
+	obs_contour   -> Smooth();
+	exp_contour   -> Smooth();
+	explo_contour -> Smooth();
+	exphi_contour -> Smooth();
+	nContSmooths--;
+}
+
+obs_contour->Draw("cont list");
+gPad->Update();
+TList * obj   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
+TList * list  = (TList *) obj->At(10);
+TGraph * grph = (TGraph*) list->First()->Clone();
+
+exp_contour->Draw("cont list");
+gPad->Update();
+TList * obj_exp   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
+TList * list_exp  = (TList *) obj_exp->At(10);
+TGraph * grph_exp = (TGraph*) list_exp->First()->Clone();
+
+explo_contour->Draw("cont list");
+gPad->Update();
+TList * obj_explo   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
+TList * list_explo  = (TList *) obj_explo->At(10);
+TGraph * grph_explo = (TGraph*) list_explo->First()->Clone();
+
+exphi_contour->Draw("cont list");
+gPad->Update();
+TList * obj_exphi   = (TList *) gROOT.GetListOfSpecials().FindObject("contours");
+TList * list_exphi  = (TList *) obj_exphi->At(10);
+TGraph * grph_exphi = (TGraph*) list_exphi->First()->Clone();
+
+TFile * template = new TFile("msugraTanb10Template.root", "READ", "template");
+TCanvas * canv = (TCanvas *) template->Get("GridCanvas");
+template->Close();
+canv->Draw();
+grph->SetLineColor(2);
+grph->SetLineWidth(3);
+
+grph_exp->SetLineColor(1);
+grph_exp->SetLineWidth(3);
+
+grph_explo->SetLineColor(1);
+grph_explo->SetLineWidth(2);
+grph_explo->SetLineStyle(2);
+
+grph_exphi->SetLineColor(1);
+grph_exphi->SetLineWidth(2);
+grph_exphi->SetLineStyle(2);
+
 //int n;
 //grph_exphi->GetN()>grph_explo->GetN() ? n=grph_explo->GetN() : n=grph_exphi->GetN();
 //TGraph * shade = new TGraph(2*n);
@@ -150,12 +133,13 @@ exphi_contour->Write();
 //shade->SetFillStyle(3003);
 //shade->SetFillColor(39);
 //shade->Draw("f");
-//
-//grph->Draw("same");
-//grph_exp->Draw("same");
-//grph_explo->Draw("same");
-//grph_exphi->Draw("same");
 
-//canv->Write();
+grph->Draw("same");
+grph_exp->Draw("same");
+grph_explo->Draw("same");
+grph_exphi->Draw("same");
+
+//file_->cd();
+canv->SaveAs("plots/exclusionPlot_crossCheck_withHiggsCombination.pdf", "");
 
 }
