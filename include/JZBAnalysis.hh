f8f4e91fcbeca962446be2f1700bfc1a28b10767
buchmann <>
2012-04-01 17:38:53 +0000

    Added loose jzb and anti selected jzb (will remove it again in a second - just for reference

--- include/JZBAnalysis.hh
+++ include/JZBAnalysis.hh
@@ -36,13 +36,14 @@ class JZBAnalysis : public UserAnalysisBase{
 public:
   JZBAnalysis(TreeReader *tr=NULL, std::string dataType="mc", bool fullCleaning=false, bool isModelScan=false, bool makeSmall=false, bool doGenInfo=false);
   virtual ~JZBAnalysis();
-  const bool IsCustomMu(const int);
-  const bool IsCustomEl(const int);
+  int IsCustomMu(const int);
+  int IsCustomEl(const int);
   const bool IsCustomPfMu(const int, const int);
   const bool IsCustomPfEl(const int, const int);
   const bool IsCustomJet(const int);
   const bool IsConvertedPhoton( const int eIndex );
   const bool passTriggers(std::vector<std::string>& triggerPaths);
+  bool DealWithLooseAndAntiSelectedLeptons(vector<lepton> sortedLooseLeptons,vector<lepton> sortedAntiSelectedLeptons);
 
   string outputFileName_; // public name of the output file name
 
