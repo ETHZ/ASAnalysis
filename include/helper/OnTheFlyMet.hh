dc80c8f29eb16f7080043bc69d56e2cde7065aed
peruzzi <>
2013-04-29 16:57:44 +0000

    This commit was manufactured by cvs2svn to create tag 'EWKino_V01-00'.
    Sprout from ra5final_b 2013-04-29 16:57:43 UTC mdunser 'first commit of the code used for the final ra5.'
    Cherrypick from ra5final_b 2013-04-20 13:04:09 UTC peruzzi 'This commit was manufactured by cvs2svn to create branch 'ra5final_b'.':
        Makefile
    Cherrypick from master 2011-09-06 09:09:41 UTC buchmann 'Added a 'small' option, which leads to uninteresting events not being stored':
        include/JZBPFAnalysis.hh
        src/JZBPFAnalysis.cc
    Cherrypick from EDMdev 2013-01-25 16:40:52 UTC fronga 'Fixes.':
        include/helper/OnTheFlyMet.hh
        src/helper/OnTheFlyMet.cc
    Delete:
        Makefile_JZB
        src/exe/ScanZBi_TTWOpt.C
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_L1FastJet_AK5PF.txt
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_L1Offset_AK5PF.txt
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_L2L3Residual_AK5PF.txt
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_L2Relative_AK5PF.txt
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_L3Absolute_AK5PF.txt
        src/helper/JetCorrectionFiles/FT_P_V42_AN3_Uncertainty_AK5PF.txt
        src/helper/JetCorrectionFiles/getCorrectorFiles.py

---
+++ include/helper/OnTheFlyMet.hh
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h"
+#include "../base/TreeReader.hh"
+
+class OnTheFlyCorrections {
+	public:
+		OnTheFlyCorrections (std::string globaltag, bool isdata);
+		~OnTheFlyCorrections(){};
+
+		std::vector<JetCorrectorParameters> fJetCorPar;
+		FactorizedJetCorrector *fJetCorrector;
+		// FactorizedJetCorrector(OnTheFlyCorrections::fJetCorPar) *fJetCorrector;
+
+  std::pair< float, float > getCorrections( float rawpt, float raweta, float rawnomupt, 
+                                            float phi, float emf, float rho, float area, std::string level = "" );
+
+		float getPx(float pt, float phi){ return pt*cos(phi); };
+		float getPy(float pt, float phi){ return pt*sin(phi); };
+};
+
