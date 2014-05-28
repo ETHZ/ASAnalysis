2c8ad1b128b150f70d36b834d33e8f1e2f04cf4e
lbaeni <lbaeni@users.github.com>
2014-05-28 10:36:44 +0200

    added top and rares to get_samples

--- python/sample.py
+++ python/sample.py
@@ -231,4 +231,8 @@ class sample :
 				if (sample.datamc > 0) : samplelist.append(sample.name)
 			elif channel == 'QCD' :
 				if (sample.getType() == 1) : samplelist.append(sample.name)
+			elif channel == 'Top' :
+				if (sample.getType() == 2) : samplelist.append(sample.name)
+			elif channel == 'Rare' :
+				if (sample.getSampleType() == 15) and (sample.name != 'TTbarW') and (sample.name != 'TTbarZ') : samplelist.append(sample.name)
 		return samplelist
