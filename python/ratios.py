dd1a4fc6530835af4256322fde0be63d21b4b518
lbaeni <lbaeni@users.github.com>
2015-01-06 14:08:59 +0100

    update on ewk fit
    - added scale factor to get_TightLoose function
    - added option to fit wjets and zjets indiviually

--- python/ratios.py
+++ python/ratios.py
@@ -149,12 +149,15 @@ class ratios :
 		## 	}
 
 
-	def get_TightLoose(self, samples, chan_str, fp) :
+	def get_TightLoose(self, samples, chan_str, fp, sf = 1., sf_dict = {}) :
 		## chan_str: 'MM', 'EE'
 
 		for i, s in enumerate(samples) :
 			scale = 1.
-			if self.samples[s].datamc != 0 : scale = self.lumi / self.samples[s].getLumi()
+			if self.samples[s].datamc != 0 :
+				sf_tmp = 1.
+				if s in sf_dict : sf_tmp = sf_dict[s]
+				scale = self.lumi / self.samples[s].getLumi() * sf * sf_tmp
 			if fp is 'SigSup' :
 				h2_ntight   = self.ssdlfile.Get(s+'/TLRatios/'+s+'_'+chan_str+'_fNTight').Clone()
 				h2_nloose   = self.ssdlfile.Get(s+'/TLRatios/'+s+'_'+chan_str+'_fNLoose').Clone()
@@ -214,7 +217,9 @@ class ratios :
 		(h_ntight_qcd  , h_nloose_qcd  ) = self.get_fRatioPlots(samples_qcd  , chan_str, 'MT_MET30')
 
 		if do_fit :
-			ratios = self.fit_fRatioPlots(h_ntight_data, h_ntight_wjets, h_ntight_zjets, h_ntight_qcd)
+			ratios = self.fit_fRatioPlots(h_ntight_data, h_ntight_wjets, h_ntight_zjets, h_ntight_qcd, individual_fit = False)
+#			ratios = self.fit_fRatioPlots(h_nloose_data, h_nloose_wjets, h_nloose_zjets, h_nloose_qcd, individual_fit = False)
+			print ratios
 			return ratios
 
 		else :
@@ -252,33 +257,46 @@ class ratios :
 		return (h_ntight, h_nloose)
 
 
-	def fit_fRatioPlots(self, h_data, h_wjets, h_zjets, h_qcd) :
+	def fit_fRatioPlots(self, h_data, h_wjets, h_zjets, h_qcd, individual_fit = True) :
 		var = ROOT.RooRealVar('var', 'var', 0., 200.)
+
+		h_ewk = h_wjets.Clone()
+		h_ewk.Add(h_zjets)
 
 		rdh_data  = ROOT.RooDataHist('data' ,'data' , ROOT.RooArgList(var), h_data , 1.)
 		rdh_wjets = ROOT.RooDataHist('wjets','wjets', ROOT.RooArgList(var), h_wjets)
 		rdh_zjets = ROOT.RooDataHist('zjets','zjets', ROOT.RooArgList(var), h_zjets)
+		rdh_ewk   = ROOT.RooDataHist('ewk'  ,'ewk'  , ROOT.RooArgList(var), h_ewk  )
 		rdh_qcd   = ROOT.RooDataHist('qcd'  ,'qcd'  , ROOT.RooArgList(var), h_qcd  )
 
 		pdf_wjets = ROOT.RooHistPdf('wjets_pdf', 'wjets_pdf', ROOT.RooArgSet(var), rdh_wjets)
 		pdf_zjets = ROOT.RooHistPdf('zjets_pdf', 'zjets_pdf', ROOT.RooArgSet(var), rdh_zjets)
+		pdf_ewk   = ROOT.RooHistPdf('ewk_pdf'  , 'ewk_pdf'  , ROOT.RooArgSet(var), rdh_ewk  )
 		pdf_qcd   = ROOT.RooHistPdf('qcd_pdf'  , 'qcd_pdf'  , ROOT.RooArgSet(var), rdh_qcd  )
 
 		init_wjets = h_wjets.Integral()
 		init_zjets = h_zjets.Integral()
+		init_ewk   = h_ewk  .Integral()
 		init_qcd   = h_qcd  .Integral()
 
 		n_wjets = ROOT.RooRealVar('n_wjets', 'number of wjets', init_wjets, 0.70*init_wjets, 1.30*init_wjets)
 		n_zjets = ROOT.RooRealVar('n_zjets', 'number of zjets', init_zjets, 0.70*init_zjets, 1.30*init_zjets)
+		n_ewk   = ROOT.RooRealVar('n_ewk'  , 'number of ewk'  , init_ewk  , 0.50*init_ewk  , 2.00*init_ewk  )
 		n_qcd   = ROOT.RooRealVar('n_qcd'  , 'number of qcd'  , init_qcd  , 0.50*init_qcd  , 2.00*init_qcd  )
 
-		model = ROOT.RooAddPdf('model', 'model', ROOT.RooArgList(pdf_wjets, pdf_zjets, pdf_qcd), ROOT.RooArgList(n_wjets, n_zjets, n_qcd))
-
+		if individual_fit :
+			model = ROOT.RooAddPdf('model', 'model', ROOT.RooArgList(pdf_wjets, pdf_zjets, pdf_qcd), ROOT.RooArgList(n_wjets, n_zjets, n_qcd))
+		else :
+			model = ROOT.RooAddPdf('model', 'model', ROOT.RooArgList(pdf_ewk, pdf_qcd), ROOT.RooArgList(n_ewk, n_qcd))
 		model.fitTo(rdh_data, ROOT.RooFit.SumW2Error(ROOT.kFALSE), ROOT.RooFit.Extended(), ROOT.RooFit.PrintLevel(-1))
 
 		scale = {}
-		scale['wjets'] = n_wjets.getVal() / init_wjets
-		scale['zjets'] = n_zjets.getVal() / init_zjets
+		if individual_fit :
+			scale['wjets'] = n_wjets.getVal() / init_wjets
+			scale['zjets'] = n_zjets.getVal() / init_zjets
+		else :
+			scale['wjets'] = n_ewk  .getVal() / init_ewk
+			scale['zjets'] = scale['wjets']
 		scale['qcd'  ] = n_qcd  .getVal() / init_qcd
 
 		return scale
